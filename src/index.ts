2207776af59ea5502569b75dd568ac123672a795
Satish Babariya <satish.babariya@gmail.com>
2022-10-02 12:50:05 +0530

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import fastify from "fastify";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const ENVIRONMENT = process.env.ENVIRONMENT;
+
+console.log(`Environment: ${ENVIRONMENT}`);
