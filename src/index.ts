1837d41107639669da1d72d6502453a62fe37250
Satish Babariya <satish.babariya@gmail.com>
2022-10-02 12:50:18 +0530

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import fastify from "fastify";
 import * as dotenv from "dotenv";
 
-// Load environment variables from .env file
-dotenv.config();
-
 const ENVIRONMENT = process.env.ENVIRONMENT;
 
 console.log(`Environment: ${ENVIRONMENT}`);
+
+// Load environment variables from .env file
+switch (ENVIRONMENT) {
+  case "development":
+    dotenv.config({ path: ".env.development" });
+    break;
+  case "production":
+    dotenv.config();
+    break;
+  default:
+    dotenv.config();
+}
