87331b39c20f8f939a179b51f049003871ca0bc6
Satish Babariya <satish.babariya@gmail.com>
2022-10-08 18:19:36 +0530

    add benchmarks and get decorators

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
-import { FastifyPluginAsync } from "fastify";
+import { FastifyInstance, FastifyPluginAsync } from "fastify";
 import { Server } from "node:http";
+import Container from "typedi";
 
 const MetadataKeys = {
   controller: "npf:controller",
@@ -9,10 +10,10 @@ const MetadataKeys = {
 
 interface ControllerMetadata {
   path: string;
-  middlewares: FastifyPluginAsync;
+  middlewares: FastifyPluginAsync[];
 }
 
-function Controller(path: string, middlewares: FastifyPluginAsync) {
+function Controller(path: string, middlewares: FastifyPluginAsync[] = []) {
   // Append / if not exist in path
   if (!path.startsWith("/")) {
     path = "/" + path;
@@ -30,4 +31,60 @@ function Controller(path: string, middlewares: FastifyPluginAsync) {
   };
 }
 
-export { Controller, ControllerMetadata };
+function routeBinder(method: string, path: string) {
+  // Append / if not exist in path
+  if (!path.startsWith("/")) {
+    path = "/" + path;
+  }
+
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    let routes = Reflect.getMetadata(MetadataKeys.httpMethod, target) || [];
+
+    routes.push({
+      method,
+      path,
+      handler: desc.value,
+    });
+
+    Reflect.defineMetadata(MetadataKeys.httpMethod, routes, target.constructor);
+  };
+}
+
+function Get(path: string) {
+  return routeBinder("GET", path);
+}
+
+function bind(fastify: FastifyInstance, controllers: any[]) {
+  controllers.forEach((controller) => {
+    const controllerMetadata = Reflect.getMetadata(
+      MetadataKeys.controller,
+      controller
+    );
+
+    if (!controllerMetadata) {
+      throw new Error(`Controller ${controller.name} is not decorated`);
+    }
+
+    const routes = Reflect.getMetadata(MetadataKeys.httpMethod, controller);
+
+    fastify.register(
+      async (instance, opts, next) => {
+        for (const route of routes) {
+          const { method, path, handler } = route;
+          console.log(route);
+          instance.route({
+            method: method,
+            url: path,
+            handler: async (request, reply) => {
+              const result = await handler(request, reply);
+              return result;
+            },
+          });
+        }
+      },
+      { prefix: controllerMetadata.path }
+    );
+  });
+}
+
+export { Controller, ControllerMetadata, Get, MetadataKeys, bind };
